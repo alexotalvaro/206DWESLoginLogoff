3e469baf71ec3a52dd000742ca63ecdba06d37cd
alexotalvaro <alejandromilles@outlook.com>
2023-01-11 10:43:02 +0100

    11/01/2022 Creación del index, Scripts de BD y carpetas de la Aplicación

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+?>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>LOGIN / LOGOFF</title>
+        <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Rajdhani:wght@300&display=swap" rel="stylesheet">
+        <link href="weboroot/css/estilos-index.css" rel="stylesheet" type="text/css">
+
+    </head>
+
+    <body>
+
+        <div>
+            <div>
+                <h1>ALEJANDRO OTÁLVARO MARULANDA</h1>
+                <h2>Aplicación Login Logoff</h2>
+            </div>
+        
+    </body>
+
+</html>
